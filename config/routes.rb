06bc02da7a5d9abd6b62cca05fd21f0f623ec7cf
OrlandoBalthazar <91983426+OrlandoBalthazar@users.noreply.github.com>
2021-10-16 23:32:02 -0300

    Hello world

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: "static_page#index"
+  #root to: "static_page#teste"
+  #root to: 'static_page#teste'
+
+end
